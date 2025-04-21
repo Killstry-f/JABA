2f85cfec2511267ed04139104a9305770b8c48a8
Killstry-f <killstry616@gmail.com>
2025-04-21 12:51:40 +0500

    Dlya Eclipse
    Proggrami

---
+++ ForEclipse/srak/src/srak/tretie.java
@@ -0,0 +1,15 @@
+package srak;
+
+public class tretie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 54;
+		System.out.print("Валера ");
+		System.out.println("54");
+		
+		System.out.print("Мамут " + age);
+	}
+
+}
---
+++ ForEclipse/srak/src/srak/vtoroe.java
@@ -0,0 +1,15 @@
+package srak;
+
+public class vtoroe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ĞÀç");
+		System.out.println("Äâà");
+		System.out.println("ÒğÈ");
+		System.out.println("÷åÒûğå");
+		System.out.println("Ïÿòü");
+		System.out.println("ØåÑòÜ");
+	}
+
+}
